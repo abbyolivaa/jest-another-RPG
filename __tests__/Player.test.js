5c3652cba7123a7a82fce7f97b440f7f60a3f7f8
Abby Oliva <abbyoliva6@icloud.com>
2022-04-26 16:02:23 -0600

    isAlive() and .reduceHealth()

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -5,6 +5,7 @@ jest.mock('../lib/Potion.js')
 console.log (new Potion());
 
 const Player = require('../lib/Player');
+const { expect } = require('expect');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -32,4 +33,27 @@ test('gets inventory from player or returns false', () => {
     expect(player.getInventory()).toEqual(expect.any(Array));
     player.inventory = [];
     expect(player.getInventory()).toEqual(false);
+});
+
+test ("gets player's health value",() => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test ('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test ("subtract from player's health", () => {
+    const player= new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth-5);
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
 });
